fc14e8d33144566ea2074e1234c646d3e861f28c
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-15 22:41:20 +0800

    style : add sales

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        roboto: "Roboto, sans-serif",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
