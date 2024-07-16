5cb2fe72b3713b5d719f37ffe83a7414aed3bcae
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-17 07:48:37 +0800

    style : list table my seles

---
+++ src/pages/my-transaction/sales-transaction/index.tsx
@@ -0,0 +1,64 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../../components/ui/table';
+import { Badge } from '../../../components/ui/badge';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from '../../../components/ui/dropdown-menu';
+import { Button } from '../../../components/ui/button';
+import { Ellipsis } from 'lucide-react';
+import { sampleMySeles } from '../../../utils/apis/my-seles/sample-data';
+import { Link } from 'react-router-dom';
+
+function SalesTransaction() {
+  return (
+    <div className="overflow-x-auto">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Order ID</TableHead>
+            <TableHead>Item</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>
+              <span className="sr-only">Actions</span>
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {sampleMySeles.map((seles) => {
+            return (
+              <TableRow key={seles.transaction_id}>
+                <TableCell className="font-medium">{seles.transaction_id}</TableCell>
+                <TableCell>{seles.product_name}</TableCell>
+                <TableCell>{seles.quantity}</TableCell>
+                <TableCell>{seles.grand_total}</TableCell>
+                <TableCell>
+                  <Badge variant="outline">{seles.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button aria-haspopup="true" size="icon" variant="ghost">
+                        <Ellipsis className="h-4 w-4" />
+                        <span className="sr-only">Toggle menu</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                      <DropdownMenuItem asChild>
+                        <Link to={'/my-transaction/detail-seles'}>
+                          View
+                        </Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>Cancel</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+export default SalesTransaction;
